87081efdc54a0e97c7b3d452bdeec96b4ba5482e
DedAzazay <welzevul0@gmail.com>
2021-09-30 17:06:28 +0300

    Changed Util class for database connection. Added working methods there.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,56 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String HOST = "jdbc:mysql://localhost:3306/mydbtest?useSSL=false";
+    private final String USER = "rood";
+    private final String PASSWORD = "rood";
+
+    private Connection connection;
+    private Statement statement;
+//    PreparedStatement preparedStatement;
+
+    public Util() {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(HOST, USER, PASSWORD);
+            statement = connection.createStatement();
+            if (!connection.isClosed()) {
+//                System.out.println("Соединение установленно");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("driver not found");
+        } catch (SQLException e) {
+            System.out.println("SQL problem with connecting +\n");
+            e.printStackTrace();
+        }
+    }
+
+    public PreparedStatement getPreparedStatement(String sql) {
+        try {
+            return this.getConnection().prepareStatement(sql);
+        } catch (SQLException e) {
+            System.out.println("SQL problem with preparedStatement");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+//    public Statement getStatement() {
+//        return statement;
+//    }
+
+    public void connectionCloser() {
+        try {
+            connection.close();
+//            System.out.println("Соединение успешно закрыто");
+        } catch (SQLException e) {
+            System.out.println("SQL problem with close");
+            e.printStackTrace();
+        }
+    }
 }
