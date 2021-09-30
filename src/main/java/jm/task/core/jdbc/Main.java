8965671657098ff37ac0977acd251663f86e0432
DedAzazay <welzevul0@gmail.com>
2021-09-30 17:08:24 +0300

    Make algorithm in Main. It's task.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Steve", "Stevanov", (byte) 22);
+        userService.saveUser("Bob", "Bobanov", (byte) 26);
+        userService.saveUser("Klark", "Klarkanov", (byte) 11);
+        userService.saveUser("Djack", "Djackanov", (byte) 73);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
