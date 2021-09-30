70ab8db29f215eab857f76e028942c4e3543816e
DedAzazay <welzevul0@gmail.com>
2021-09-30 17:34:16 +0300

    Resolve conflict from SQLException in UserService interface

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
     public void createUsersTable() {
 
     }
