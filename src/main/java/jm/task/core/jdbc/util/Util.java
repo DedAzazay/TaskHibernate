e79c08669c36cc244bb17d8d4f35ea2034fb2047
DedAzazay <welzevul0@gmail.com>
2021-11-05 02:35:30 +0300

    Add Hibernate methods in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
     private final String DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -9,16 +17,52 @@ public class Util {
     private final String PASSWORD = "rood";
 
     private Connection connection;
-    private Statement statement;
-//    PreparedStatement preparedStatement;
 
     public Util() {
+
+    }
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    public static SessionFactory buildSessionFactory() {
+        try {
+            Configuration cfg = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest?useSSL=false");
+            settings.put(Environment.USER, "rood");
+            settings.put(Environment.PASS, "rood");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.POOL_SIZE, 1);
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.USE_REFLECTION_OPTIMIZER,"false");
+            settings.put(Environment.HBM2DDL_AUTO, "update");
+            cfg.setProperties(settings);
+            cfg.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(cfg.getProperties()).build();
+            return cfg.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+
+    public void getJDBCUtil() {
         try {
             Class.forName(DRIVER);
             connection = DriverManager.getConnection(HOST, USER, PASSWORD);
-            statement = connection.createStatement();
+            connection.createStatement();
             if (!connection.isClosed()) {
-//                System.out.println("Соединение установленно");
+
             }
         } catch (ClassNotFoundException e) {
             System.out.println("driver not found");
@@ -27,6 +71,7 @@ public class Util {
             e.printStackTrace();
         }
     }
+
 
     public PreparedStatement getPreparedStatement(String sql) {
         try {
@@ -40,9 +85,7 @@ public class Util {
     public Connection getConnection() {
         return connection;
     }
-//    public Statement getStatement() {
-//        return statement;
-//    }
+
 
     public void connectionCloser() {
         try {
