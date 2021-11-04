6a601fd491b966f27a9a770f6e5237127c7323b9
DedAzazay <welzevul0@gmail.com>
2021-11-05 02:35:56 +0300

    Make some tests just for me

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Steve", "Stevanov", (byte) 22);
-        userService.saveUser("Bob", "Bobanov", (byte) 26);
-        userService.saveUser("Klark", "Klarkanov", (byte) 11);
-        userService.saveUser("Djack", "Djackanov", (byte) 73);
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+//        userDaoHibernate.createUsersTable();
+//        userDaoHibernate.saveUser("Steve", "Stevanov", (byte) 22);
+//        userDaoHibernate.saveUser("Bob", "Bobanov", (byte) 26);
+//        userDaoHibernate.saveUser("Klark", "Klarkanov", (byte) 11);
+//        userDaoHibernate.saveUser("Djack", "Djackanov", (byte) 73);
+//        userDaoHibernate.removeUserById(3L);
+//        List<User> list = userDaoHibernate.getAllUsers();
+//        for(User people : list) {
+//            System.out.println(people);
+//        }
+//        userDaoHibernate.dropUsersTable();
+//        userDaoHibernate.cleanUsersTable();
     }
 }
