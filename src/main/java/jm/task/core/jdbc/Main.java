922d689f03073a48e5811d7221589299c47a1e02
DedAzazay <welzevul0@gmail.com>
2021-11-05 03:16:27 +0300

    Do finale exercise

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,24 +2,28 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-//        userDaoHibernate.createUsersTable();
-//        userDaoHibernate.saveUser("Steve", "Stevanov", (byte) 22);
-//        userDaoHibernate.saveUser("Bob", "Bobanov", (byte) 26);
-//        userDaoHibernate.saveUser("Klark", "Klarkanov", (byte) 11);
-//        userDaoHibernate.saveUser("Djack", "Djackanov", (byte) 73);
-//        userDaoHibernate.removeUserById(3L);
-//        List<User> list = userDaoHibernate.getAllUsers();
-//        for(User people : list) {
-//            System.out.println(people);
-//        }
-//        userDaoHibernate.dropUsersTable();
-//        userDaoHibernate.cleanUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        int i = 0;
+        userService.saveUser("Steve", "Stevanov", (byte) 22);
+        System.out.println("User s imenen " + userService.getAllUsers().get(i++).getName() + " dobavlen v BD");
+        userService.saveUser("Bob", "Bobanov", (byte) 26);
+        System.out.println("User s imenen " + userService.getAllUsers().get(i++).getName() + " dobavlen v BD");
+        userService.saveUser("Klark", "Klarkanov", (byte) 11);
+        System.out.println("User s imenen " + userService.getAllUsers().get(i++).getName() + " dobavlen v BD");
+        userService.saveUser("Djack", "Djackanov", (byte) 73);
+        System.out.println("User s imenen " + userService.getAllUsers().get(i++).getName() + " dobavlen v BD");
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.shutdown();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -34,7 +34,7 @@ public class Util {
             settings.put(Environment.PASS, "rood");
             settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
             settings.put(Environment.POOL_SIZE, 1);
-            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.SHOW_SQL, "false");
             settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
             settings.put(Environment.USE_REFLECTION_OPTIMIZER,"false");
             settings.put(Environment.HBM2DDL_AUTO, "update");
